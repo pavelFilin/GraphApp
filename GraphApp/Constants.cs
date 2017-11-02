86532608db616197448c58ba5635179f6236c158
Павел Филин <pavel164rus1@gmail.com>
2017-11-02 11:07:46 +0400

    Add project files.

---
+++ GraphApp/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphApp
+{
+    static class Constants
+    {
+
+        public static int PixelSize
+        {
+            get { return pixelSize; }
+            set { pixelSize = value; }
+        }
+
+
+   
+
+        private static int pixelSize = 3;
+    }
+}
---
+++ GraphApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            log = new Logic();
+            Constants.PixelSize = pictureBox1.Width / log.Pixels.GetLength(0);
+            pictureBox1.Paint += DrawPixels;
+        }
+
+        private Logic log;
+        
+        private void DrawPixels(object sender, PaintEventArgs e)
+        {
+            for(int i = 0; i < log.Pixels.GetLength(0); i++)
+            {
+                for(int j = 0; j < log.Pixels.GetLength(1); j++)
+                {
+                    if (log.Pixels[i, j])
+                    {
+                        e.Graphics.FillRectangle(Brushes.Black, i * Constants.PixelSize, j * Constants.PixelSize, Constants.PixelSize, Constants.PixelSize);
+                    }
+                    else
+                    {
+                        e.Graphics.FillRectangle(Brushes.White, i * Constants.PixelSize, j * Constants.PixelSize, Constants.PixelSize, Constants.PixelSize);
+                    }
+                   
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            log.DrawLine(5, 5, 50, 110);
+            pictureBox1.Invalidate();
+        }
+    }
+}
